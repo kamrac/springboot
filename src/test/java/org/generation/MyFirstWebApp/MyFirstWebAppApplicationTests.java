542976991d575b4c4d3006c046dd4eb87b09c67c
Kamra <kamra.c@gmail.com>
2022-11-30 19:47:59 +0000

    first spring boot project

---
+++ src/test/java/org/generation/MyFirstWebApp/MyFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.MyFirstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
